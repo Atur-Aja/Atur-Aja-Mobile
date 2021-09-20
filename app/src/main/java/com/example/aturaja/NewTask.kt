256978a711e9a9d8a0baf1446fbf689f63fe7a0f
Faza Ghulam Ahmad <fazaghulam@gmail.com>
2021-09-20 16:02:03 +0700

    add new task layout

--- app/src/main/java/com/example/aturaja/NewTask.kt
+++ app/src/main/java/com/example/aturaja/NewTask.kt
@@ -1,12 +1,11 @@
 package com.example.aturaja
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NewTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_new_task)
     }
 }
