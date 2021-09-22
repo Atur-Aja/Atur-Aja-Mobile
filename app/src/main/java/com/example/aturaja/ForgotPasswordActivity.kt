0f5d8f8bf65ee0c5f8d5839d546da8d8594504cc
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2021-09-22 10:47:46 +0700

    Menambahkan Layout Forgot Password

---
+++ app/src/main/java/com/example/aturaja/ForgotPasswordActivity.kt
@@ -0,0 +1,18 @@
+package com.example.aturaja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+
+    fun resetOnClick(view: android.view.View) {
+
+    }
+    fun backOnClick(view: android.view.View) {
+
+    }
+}
