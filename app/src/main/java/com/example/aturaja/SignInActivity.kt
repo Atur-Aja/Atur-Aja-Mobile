e39522c129329e3fc0db9ee9286d39128dbebf6f
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2021-09-23 14:35:25 +0700

    Menambahkan icon pada editText dan menambahkan intent

--- app/src/main/java/com/example/aturaja/SignInActivity.kt
+++ app/src/main/java/com/example/aturaja/SignInActivity.kt
@@ -1,11 +1,22 @@
 package com.example.aturaja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+    }
+
+    fun signUpOnClick(view: android.view.View) {
+
+    }
+    fun forgotPasswordOnClick(view: android.view.View) {
+        val intent = Intent(this, ForgotPasswordActivity::class.java)
+        startActivity(intent)
+
     }
 }
