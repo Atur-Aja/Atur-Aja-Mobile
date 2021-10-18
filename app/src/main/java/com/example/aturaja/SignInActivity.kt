c376df8082cc8497542eb1fb53ccfb66e261daf1
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2021-10-18 16:30:34 +0700

    Show/hide password

--- app/src/main/java/com/example/aturaja/SignInActivity.kt
+++ app/src/main/java/com/example/aturaja/SignInActivity.kt
@@ -6,17 +6,28 @@ import android.os.Bundle
 
 
 class SignInActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
     }
 
     fun signUpOnClick(view: android.view.View) {
 
     }
+
     fun forgotPasswordOnClick(view: android.view.View) {
         val intent = Intent(this, ForgotPasswordActivity::class.java)
         startActivity(intent)
 
     }
+
+    fun signInOnClick(view: android.view.View) {
+        val intent = Intent(this, CalendarActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
