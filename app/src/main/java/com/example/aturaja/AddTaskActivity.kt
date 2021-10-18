6b224f4f06d1c6a7e6b72278528c20e54f7f3dc9
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2021-10-18 16:38:09 +0700

    Menambahkan Layout Add task

--- app/src/main/java/com/example/aturaja/AddTaskActivity.kt
+++ app/src/main/java/com/example/aturaja/AddTaskActivity.kt
@@ -1,13 +1,15 @@
 package com.example.aturaja
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AddTaskActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_add_task)
 
     }
+
+
 }
