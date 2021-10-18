0fa22820e6a9c41b366ec34edcd42494e59f66a7
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2021-10-18 23:34:45 +0700

    Penambahan adapter dan data class

--- app/src/main/java/com/example/aturaja/AddTaskActivity.kt
+++ app/src/main/java/com/example/aturaja/AddTaskActivity.kt
@@ -1,13 +1,46 @@
 package com.example.aturaja
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArrayList: ArrayList<Todo>
+    private lateinit var todoName : Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        todoName = arrayOf(
+            "tidur",
+            "tidur",
+        )
+
+
+        newRecyclerView = findViewById(R.id.toDoRecyclerView)
+        newRecyclerView.layoutManager = LinearLayoutManager(this)
+        newRecyclerView.setHasFixedSize(true)
+
+        newArrayList = arrayListOf()
+        getUserData()
+
+    }
+
+    private fun getUserData() {
+
+        for(i in todoName.indices){
+
+            val todo = Todo(todoName[i])
+            newArrayList.add(todo)
+
+        }
+
+        newRecyclerView.adapter = TodoAdapter(newArrayList)
 
     }
 
---
+++ app/src/main/java/com/example/aturaja/Todo.kt
@@ -0,0 +1,3 @@
+package com.example.aturaja
+
+data class Todo(var toDoName : String)
