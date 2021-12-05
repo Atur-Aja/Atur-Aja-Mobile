fdd9dbb5cfca75990a37d3905603bf873a7fdd67
Muhammad Bima <2110191059@msoffice.pens.ac.id>
2021-12-05 20:10:58 +0700

    penambahan edit profile

---
+++ app/src/main/java/com/example/aturaja/profile/SetupProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aturaja.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aturaja.R
+
+class SetupProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup_profile)
+    }
+}
